4ff5f964c87060de9db44453c16561a6b1b6d18d
Nihal Uikey <nihaluikey6488@gmail.com>
2026-05-29 14:16:59 +0530

    Confiure server

---
+++ server.js
@@ -0,0 +1,16 @@
+/**
+ * This file is responsible for starting the server and listening on a specific port. It imports the configured Express application from app.js and starts the server.
+ */
+// Importing the configured express application from app.js
+import dotenv from 'dotenv' 
+
+import app from "./src/app.js";
+// Load Envirment variables from .env file
+dotenv.config() //con
+// set the port to listen on, either from environment variable or default to 4000
+let port=process.env.PORT 
+
+// Starting Express server
+app.listen(port,()=>{
+   console.log(`Server is running on port ${port}`) 
+})
---
+++ src/app.js
@@ -0,0 +1,10 @@
+/**
+ * This is file is setting the application of Express server
+ */
+import express from 'express' 
+
+// Initialize Express Application
+let app=express()
+
+// Export Configured app so we can use it in  other file  
+export default app;
